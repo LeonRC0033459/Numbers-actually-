e0aa0d58170df3aacd08d261a6f5ca9d4926510a
McGrath <rc0033459@student.riversidecollege.ac.uk>
2020-02-11 12:34:57 +0000

    Add project files.

---
+++ Numbers (actually)/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numbers__actually_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double d1 = 5e6;
+            richTextBox1.AppendText(d1.ToString() + "\n");
+            richTextBox1.AppendText(d1.ToString("N") + "\n");
+            richTextBox1.AppendText(d1.ToString("N3") + "\n");
+            richTextBox1.AppendText(d1.ToString("N0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
+            richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+
+            double pi = 3.141592653589793;
+            richTextBox1.AppendText(pi.ToString());
+            richTextBox1.AppendText(3.ToString());
+        }
+    }
+}
